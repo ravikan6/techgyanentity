ef9d56e45da29056cccdcd08dfddae781d3db80e
Hansraj Saini <113160822+Hansraja@users.noreply.github.com>
2024-06-13 12:29:10 +0000

    Hi

---
+++ src/lib/actions/.ipynb_checkpoints/blog-checkpoint.jsx
@@ -0,0 +1,14 @@
+"use server";
+import { prisma } from '@/lib/db';
+
+export const getBlogs = async () => {
+
+    // const dt = await prisma.post.create({
+    //     data: {
+    //         slug: 'the-blog-post',
+    //         title: "Blog Post 1",
+    //         body: "This is the content of Blog Post 1"
+    //     }})
+
+    return {};
+};
